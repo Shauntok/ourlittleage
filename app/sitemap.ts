46acaf9efdbf84ace7c5aa9356d4af618f455233
Shauntok <shauntok@gmail.com>
2026-06-16 04:50:06 +0800

    feat: add sitemap for SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,16 +3,40 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.ourlittleage.com",
+      url: "https://ourlittleage.com",
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "daily",
       priority: 1,
     },
     {
-      url: "https://www.ourlittleage.com/announcements",
+      url: "https://ourlittleage.com/home",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: "https://ourlittleage.com/space",
+      lastModified: new Date(),
+      changeFrequency: "hourly",
+      priority: 0.9,
+    },
+    {
+      url: "https://ourlittleage.com/articles",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://ourlittleage.com/diary",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://ourlittleage.com/announcements",
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.6,
+      priority: 0.7,
     },
   ];
 }
